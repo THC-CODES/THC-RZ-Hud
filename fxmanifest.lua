37a9b5d3b6677ffed0e68e7a5830d8494a4db30d
THC <88347509+THCVP@users.noreply.github.com>
2022-03-17 08:44:36 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'THC'
+description 'Redzone Hud'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+client_script {
+    'client/client.lua'
+}
